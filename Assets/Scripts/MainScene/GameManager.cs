464604a7fde08a015f1db8c29f85355b9a8933ca
田村慧人 <tamurakeito@chibikeinoMacBook-Air.local>
2022-04-20 10:56:57 +0900

    ダンジョン選択時の動作のタップアクション

--- Assets/Scripts/MainScene/GameManager.cs
+++ Assets/Scripts/MainScene/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,19 +9,15 @@ public class GameManager : MonoBehaviour
     public GameObject objectFireDungeon;
     public GameObject objectfireDungeon01;
     public GameObject selectDungeon;
+    public GameObject objectEntranceDungeon;
     public GameObject canvasGame;
 
-    // Start is called before the first frame update
     void Start()
     {
         objectNomalDungeon.GetComponent<Animator>().SetTrigger("slideIn");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //DungeonObjectディレクトのスクリプトから呼び出し
 
     public void TapNomalDungeon(){
         //nomalの消去
@@ -38,5 +35,29 @@ public class GameManager : MonoBehaviour
     
     public void TapfireDungeon01(){
         selectDungeon.SetActive(true);
+    }
+
+    //SelectDungeonディレクトのスクリプトから呼び出し
+
+    public void TapEnterButton(){
+        // DungeonSceneの追加
+        SceneManager.LoadScene ("DungeonScene");
+	    // MainSceneの削除
+        SceneManager.UnloadSceneAsync("MainScene");
+    }
+
+    public void TapBackButton(){
+        //shrinkアニメーション
+        objectEntranceDungeon.GetComponent<Animator>().SetTrigger("shrink");
+        //オブジェクト消去
+        StartCoroutine(BackDungeonMenu());
+    }
+
+    IEnumerator BackDungeonMenu()
+    {
+        // 指定した秒数だけ処理を待つ(ここでは0.1秒)
+        yield return new WaitForSeconds(0.5f);
+        //オブジェクトを消去
+        selectDungeon.SetActive(false);
     }
 }
--- Assets/Scripts/TitleScene/Title.cs
+++ Assets/Scripts/TitleScene/Title.cs
@@ -5,27 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     // ボタンを押した時の処理
     public void Tap()
     {
         // MainSceneの追加
         SceneManager.LoadScene ("MainScene");
 
-	// TitleSceneの削除
+	    // TitleSceneの削除
         SceneManager.UnloadSceneAsync("TitleScene");
-	
-	Debug.Log("デバッグ");
     }
 }
