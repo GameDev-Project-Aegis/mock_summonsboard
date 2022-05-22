7622ad55a271537d801ae38018904bb4281a9eef
shuya0908 <shuya.0908@icloud.com>
2022-05-22 16:47:05 +0900

    アニメーションの作成（Game Startの表示）

---
+++ Assets/Scripts/DungeonScene/GameStartEffect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStartEffect : MonoBehaviour
+{
+    public GameObject imageGame;
+    public GameObject imageStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
